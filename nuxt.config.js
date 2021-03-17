90864ddfb760051c80b182e6b180675f3738f521
NixFront <ya.nik34@yandex.ru>
2021-03-17 16:03:57 +0300

    v 0.5

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,9 +9,11 @@ export default {
       { hid: 'title', name: 'title', content: 'Crypto Course' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'apple-touch-icon', type:'image/x-icon', sizes:"180x180", href: '/apple-touch-icon.png' },
+      { rel: 'icon', type:'image/png', sizes:"32x32", href:'/favicon-32x32.png' },
+      { rel: 'icon', type:'image/png', sizes:"16x16", href:'/favicon-16x16.png' },
       { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
-      { rel: 'stylesheet', href: "https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" },
+      { rel: 'stylesheet', href: "https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" }
     ]
   },
   css: [
